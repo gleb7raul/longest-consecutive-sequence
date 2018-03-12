0c296ff100cd00066a3c3390c8b1154c7dd2532f
gleb7raul <glebimptab@gmail.com>
2018-03-12 18:44:05 +0200

    TaskDone

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  if(array.length == 0 ) return 0;
+  if(array.length == 1 ) return 1;
+  var res = 0;
+  var tmp = 1;
+  array.sort(function(x,y){ return x-y;});
+  for(i=1;i<array.length;i++){
+    if(array[i]-array[i-1]==1){
+      tmp++;
+    }
+    if(array[i]-array[i-1]==0){
+      continue;
+    }
+    if(array[i]-array[i-1]>1){
+      res = (tmp>res)? tmp : res ;
+      tmp = 1;
+    }
+  }
+return res;
 }
